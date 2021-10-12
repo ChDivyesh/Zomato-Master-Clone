739cc3cda57ed721ac4c001f7a589838ca9c1908
divyesh-ram22 <Divyeshram753@gmail.com>
2021-10-12 12:04:39 +0530

    created client side part and added navbar

---
+++ client/src/Layout/Home.layout.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+//Components
+import Navbar from "../Components/Navbar";
+
+
+const HomeLayout = (props) => {
+ return(
+     <>
+     <div className="container mx-auto lg:px-20">
+        <Navbar/>
+     {props.children}
+     </div>
+      
+     </>
+ );
+};
+
+export default HomeLayout;
