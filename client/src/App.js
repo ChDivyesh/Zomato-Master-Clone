376b56b19f425a065acc3a4af165a66e105c983d
divyesh-ram22 <Divyeshram753@gmail.com>
2021-10-13 11:56:30 +0530

    addedrestaurant overview

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import RestaurantLayoutHOC from "./HOC/Restaurant.HOC";
 
 //Component
 import Temp from "./Components/temp";
+import Overview from "./Page/Restaurant/Overview";
 
 // pages
 import Home from "./Page/Home";
@@ -16,12 +17,15 @@ function App() {
     <Route path="/" exact>
         <Redirect to="/delivery" />
       </Route>
+      <Route path="/restaurant/:id" exact>
+        <Redirect to="/restaurant/:id/overview" />
+      </Route>
       <HomeLayoutHOC path="/:type" exact component={Home} />
-      <RestaurantLayoutHOC path="/restaurant/:id" exact component={Temp} />
+      
       <RestaurantLayoutHOC
         path="/restaurant/:id/overview"
         exact
-        component={Temp}
+        component={Overview}
       />
       <RestaurantLayoutHOC
         path="/restaurant/:id/order-online"
